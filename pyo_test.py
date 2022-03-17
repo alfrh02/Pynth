6944d4fd10d68a4d29be52b3e85b1c9031fa4015
alfrh02 <vulcanic101@gmail.com>
2022-03-17 14:25:06 +0000

    pc push

--- pyo_test.py
+++ pyo_test.py
@@ -7,4 +7,8 @@ lfd = Sine([.4,.3], mul=.2, add=.5)
 lf2 = Sine(freq=.25, mul=10, add=30)
 
 while True:
-    lfd.out()
+	time.sleep(2)
+	saw = SuperSaw(freq=440,detune=lfd,bal=0.5,mul=0.2).out()
+
+s.stop()
+
